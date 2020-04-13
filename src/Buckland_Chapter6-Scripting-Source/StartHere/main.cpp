871b48ac56ec9c0ef5a0d80e300fb1830623a90c
Lars-Gunnar West <lars-gunnar.west@saabgroup.com>
2020-04-13 18:23:02 +0200

    added first lua test

--- src/Buckland_Chapter6-Scripting-Source/StartHere/main.cpp
+++ src/Buckland_Chapter6-Scripting-Source/StartHere/main.cpp
@@ -1,34 +1,17 @@
- extern "C"
-{
-  #include <lua.h>
-  #include <lualib.h>
-  #include <lauxlib.h>
-}
-
-//include the lua libraries. If your compiler doesn't support this pragma
-//then don't forget to add the libraries in your project settings!
-//#pragma comment(lib, "lua.lib")
-//#pragma comment(lib, "lualib.lib")
- #pragma comment(lib, "lua5.1.lib")
-
+#include "lua.hpp"
 #include <iostream>
+
+#include "this_dir.h"
 
 int main()
 {
   //create a lua state
-  lua_State* pL = lua_open();
-
-  //enable access to the standard libraries
- /* luaopen_base(pL);
-  luaopen_string(pL);
-  luaopen_table(pL);
-  luaopen_math(pL);
-  luaopen_io(pL);*/
+  lua_State* L = luaL_newstate();
 
   //open the lua libaries - new in lua5.1
-  luaL_openlibs(pL);
+  luaL_openlibs(L);
   
-  if (int error = luaL_dofile(pL, "your_first_lua_script.lua") != 0)
+  if (int error = luaL_dofile(L, THIS_DIR "your_first_lua_script.lua") != 0)
   {
     std::cout << "\n[C++]: ERROR(" << error << "): Problem with lua script file!\n\n" << std::endl;
 
@@ -36,7 +19,7 @@ int main()
   }
 
   //tidy up
-  lua_close(pL);
+  lua_close(L);
 
   return 0;
 }
