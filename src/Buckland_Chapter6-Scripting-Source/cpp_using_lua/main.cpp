fbf746297b8c4e799cb1feb9e6ab4dc613acc3ae
Lars-Gunnar West <lars-gunnar.west@saabgroup.com>
2020-04-13 19:02:09 +0200

    added cpp_using_lua, renamed some projects

--- src/Buckland_Chapter6-Scripting-Source/cpp_using_lua/main.cpp
+++ src/Buckland_Chapter6-Scripting-Source/cpp_using_lua/main.cpp
@@ -1,53 +1,44 @@
-extern "C"
-{
-  #include <lua.h>
-  #include <lualib.h>
-  #include <lauxlib.h>
-}
-
-#pragma comment(lib, "lua5.1.lib")
-//#pragma comment(lib, "lua.lib")
-//#pragma comment(lib, "lualib.lib")
-
+#include "lua.hpp"
 #include <iostream>
 #include <string>
 using namespace std;
 
-#include "LuaHelperFunctions.h"
+#include "luahelper/LuaHelperFunctions.h"
 
+#include "this_dir.h"
 
 
 int main()
 {
   //create a lua state
-  lua_State* pL = lua_open();
+  lua_State* L = luaL_newstate();
 
   //open the libraries - new in Lua5.1
-  luaL_openlibs(pL);
+  luaL_openlibs(L);
   
-  RunLuaScript(pL, "cpp_using_lua.lua");
+  RunLuaScript(L, THIS_DIR"cpp_using_lua.lua");
  
   cout << "\n[C++]:  1. Assigning lua string and number types to C++ std::string & int types\n";
   
   //reset the stack index
-  lua_settop(pL, 0);
+  lua_settop(L, 0);
 
   //put the global variables 'age' and 'name' on the stack.
-  lua_getglobal(pL, "age");
-  lua_getglobal(pL, "name");
+  lua_getglobal(L, "age");
+  lua_getglobal(L, "name");
 
   //check that the variables are the correct type. (notice how the 
   //stack index starts at 1, not 0)
-  if (!lua_isnumber(pL, 1) || !lua_isstring(pL, 2))
+  if (!lua_isnumber(L, 1) || !lua_isstring(L, 2))
   {
     cout << "\n[C++]: ERROR: Invalid type!";
   }
 
   //now assign the values to C++ variables
-  string name = lua_tostring(pL, 2);
+  string name = lua_tostring(L, 2);
 
   //notice the cast to int with this.
-  int    age = (int)lua_tonumber(pL, 1);
+  int    age = (int)lua_tonumber(L, 1);
 
   cout << "\n\n[C++]: name = " << name 
        << "\n[C++]: age  = " << age << endl;
@@ -60,9 +51,9 @@ int main()
 
 
   //put the table on the stack
-  lua_getglobal(pL, "simple_table");
+  lua_getglobal(L, "simple_table");
 
-  if (!lua_istable(pL, -1))
+  if (!lua_istable(L, -1))
   {
     cout << "\n[C++]: ERROR: simple_table is not a valid table";
   }
@@ -70,38 +61,38 @@ int main()
   else
   {
     //push the key onto the stack
-    lua_pushstring(pL, "name");
+    lua_pushstring(L, "name");
 
     //table is now at -2 (key is at -1). lua_gettable now pops the key off
     //the stack and then puts the data found at the key location on the stack
-    lua_gettable(pL, -2);
+    lua_gettable(L, -2);
 
     //check that is the correct type
-    if (!lua_isstring(pL, -1))
+    if (!lua_isstring(L, -1))
     {
       cout << "\n[C++]: ERROR: invalid type";
     }
 
     //grab the data
-    name = lua_tostring(pL, -1);
+    name = lua_tostring(L, -1);
 
     cout << "\n\n[C++]: name = " << name;
 
-    lua_pop(pL, 1);
+    lua_pop(L, 1);
 
     /* now to do the same for the age */
 
-    lua_pushstring(pL, "age");
-    lua_gettable(pL, -2);
-    if (!lua_isnumber(pL, -1))
+    lua_pushstring(L, "age");
+    lua_gettable(L, -2);
+    if (!lua_isnumber(L, -1))
     {
       cout << "\n[C++]: ERROR: invalid type";
     }
 
     //grab the data
-    age = (int)lua_tonumber(pL, -1);
+    age = (int)lua_tonumber(L, -1);
 
-    lua_pop(pL, 1);
+    lua_pop(L, 1);
 
     cout << "\n[C++]: age  = " << age;
     
@@ -112,27 +103,27 @@ int main()
    cout << "\n\n[C++]: 3. Calling a simple Lua function: add(a,b)";
 
    //get the function from the global table and push it on the stack
-   lua_getglobal(pL, "add");
+   lua_getglobal(L, "add");
 
    //check that it is there
-   if (!lua_isfunction(pL, -1))
+   if (!lua_isfunction(L, -1))
    {
      cout << "\n\n[C++]: Oops! The lua function 'add' has not been defined";
    }
 
    //push some variables onto the lua stack
-   lua_pushnumber(pL, 5);
-   lua_pushnumber(pL, 8);
+   lua_pushnumber(L, 5);
+   lua_pushnumber(L, 8);
 
    //calling the function with parameters to set the number of parameters in
    //the lua func and how many return values it returns. Puts the result at
    //the top of the stack.
-   lua_call(pL, 2, 1);
+   lua_call(L, 2, 1);
 
    //grab the result from the top of the stack
-   int result = (int)lua_tonumber(pL, -1);
+   int result = (int)lua_tonumber(L, -1);
 
-   lua_pop(pL, 1);
+   lua_pop(L, 1);
 
    cout << "\n\n[C++]: <lua>add(5,8) = " << result;
 
@@ -140,7 +131,7 @@ int main()
 
   
   //tidy up
-  lua_close(pL);
+  lua_close(L);
 
   cout << "\n\n\n";
     
